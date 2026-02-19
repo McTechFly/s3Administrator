129c9be292bae52c6d0969abeb4fb353d6a1b9e9
Taha Yusuf Kömür <tahayusufkomur@gmail.com>
2026-02-19 22:39:50 +0100

    remove flags from db

---
+++ prisma/migrations/20260219170000_drop_plan_file_operation_flags/migration.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "Plan"
+  DROP COLUMN "transferTasks",
+  DROP COLUMN "recursiveDelete",
+  DROP COLUMN "multipleUpload",
+  DROP COLUMN "copyFolderToFolder",
+  DROP COLUMN "copyBucketToBucket",
+  DROP COLUMN "searchAllFiles",
+  DROP COLUMN "syncTasks";
